deb1f6701b8468de4ded7b649dd6b2461050d263
reinaldosantosrosa <rei16703@gmail.com>
2024-09-24 19:45:43 -0300

    Implementando services Person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 import {MatToolbarModule} from '@angular/material/toolbar';
 
+=======
+import { ContactService } from './services/contact.service';
+>>>>>>> 3a6ffda8bd07643f4dc98cc6b6f40eb0776d1865
 
 @Component({
   selector: 'app-root',
@@ -10,6 +14,24 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+<<<<<<< HEAD
 export class AppComponent {
   title = 'EscolaOnLine';
+=======
+
+export class AppComponent  implements OnInit {
+  title = 'Escola Online'
+  private contactList: any[] = []
+  constructor(private contactService: ContactService){    
+  }
+
+  ngOnInit(): void {   
+    this.contactService.findAll().subscribe( (result) => this.contactList = result)
+  }
+
+  createContact() {
+    const data = { contactType: 'MOBILE', value: '43 9 1234 5678', notes: 'Principal Mobile', isMain: true }
+    this.contactService.create(data)
+  }  
+>>>>>>> 3a6ffda8bd07643f4dc98cc6b6f40eb0776d1865
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+<<<<<<< HEAD
 import { provideHttpClient, withFetch }  from '@angular/common/http' ;
 
 
@@ -10,4 +11,15 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(),
     provideHttpClient(withFetch())]
+=======
+import { provideHttpClient } from '@angular/common/http';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideClientHydration(),
+    provideHttpClient()
+  ]
+>>>>>>> 3a6ffda8bd07643f4dc98cc6b6f40eb0776d1865
 };
